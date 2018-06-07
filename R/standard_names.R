e9382a677434c01ffd8cb6933e6e271bf4874c21
zsmith27 <zsmith@icprb.org>
2018-06-07 08:49:25 -0400

    Added remove.non.alphanumeric.char to make this function just convert column names to lowercase and remove leading/trailing white space. Also replaced ... with replacement.char to be more specific.

--- R/standard_names.R
+++ R/standard_names.R
@@ -1,7 +1,8 @@
 #' @title Standard Column Names
 #' @description Standardizes column names by converting the strings to lowercase, removing white space, and removing all non-alphanumeric characters and replacing them with a specified character.
 #' @param x A data frame or tibble.
-#' @inheritParams replace_non_alphanumeric_char
+#' @param remove.non.alphanumeric.char If FALSE, all non-alphanumeric characters will be unaltered. If TRUE, all non-alphanumeric characters will be replaced by a specified character. Default: TRUE
+#' @param replacement.char The character(s) used to replace all of the non-alphanumeric characters., Default: '_'
 #' @return A data fram with standardized column names.
 #' @details DETAILS
 #' @examples 
@@ -15,12 +16,15 @@
 #' }
 #' @rdname standard_names
 #' @export 
-standard_names <- function(x, ...) {
+standard_names <- function(x, remove.non.alphanumeric.char = TRUE, replacement.char = "_") {
   if (!is.data.frame(x)) stop("'x' must be a data frame or tibble")
   
   names(x) <- tolower(names(x))
   names(x) <- trimws(names(x))
-  names(x) <- replace_non_alphanumeric_char(names(x), ...)
+  if (remove.non.alphanumeric.char == TRUE) {
+    names(x) <- replace_non_alphanumeric_char(names(x), replacement.char)
+  }
+  
     
   return(x)
 }
