22c64bd74b130f8824b5e3ee4ddf7c7aad14edc9
zsmith27 <zsmith@icprb.org>
2018-07-18 09:57:04 -0400

    Provide the option to drop columns that only contain NAs, and therefore are useless.

--- R/prep_df.R
+++ R/prep_df.R
@@ -3,6 +3,7 @@
 #' @param x PARAM_DESCRIPTION
 #' @param standard.names Should column names be standardized?, Default: TRUE
 #' @param standard.char Should character column strings be standardized?, Default: TRUE
+#' @param drop.na.cols Should columns containing only NAs be dropped?, Default: FALSE
 #' @inheritParams standard_names
 #' @return A standardized data frame.
 #' @details DETAILS
@@ -18,11 +19,12 @@
 #' @rdname prep_df
 #' @export 
 
-prep_df <- function(x, standard.names = TRUE, standard.char = TRUE, ...){
+prep_df <- function(x, standard.names = TRUE, standard.char = TRUE, drop.na.cols = FALSE, ...){
   if (!is.data.frame(x)) stop("'x' must be a data frame or tibble")
   final.df <- x
   if (standard.names == TRUE) final.df <- standard_names(final.df, ...)
   if (standard.char == TRUE) final.df <- standard_char(final.df, ...)
+  if (drop.na.cols == TRUE) final.df <- dplyr::filter(final.df, !rowSums(is.na(.)) == ncol(.))
   # final.df[final.df == ""] <- NA
   return(final.df)
 }
